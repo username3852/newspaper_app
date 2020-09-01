f0b47b6282f1ba4044519f6313feb7a97e2d0b20
username3852 <username3852@gmail.com>
2020-09-01 17:30:38 +0545

    User Authentication and Password Change

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser  # to create custom user
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    ROLES = (("0", "Admin"), ("1", "Journalist"), ("2", "Guest"))
+    role = models.CharField(max_length=1, choices=ROLES, default="2")
+    email = models.EmailField(
+        verbose_name="email address", max_length=255, unique=True,)
+    age = models.PositiveIntegerField(null=False, blank=False)
+
+    # when admin/ opens in browser the sign in comes with email not username
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = "username", "role", "first_name", "last_name", "age"
+
+    def __str__(self):
+        return self.username
